4f42bb09207e9ff719360ef00f349ae0916a15e9
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-16 14:19:34 +0100

    ft(signup.html-accountType)
    *Added script in app.js to display bitcoin payment form on selection of admin account type
    *Designed payment input form in signup.html to be displayed when admin account type is selected

--- Template/resources/scripts/app.js
+++ Template/resources/scripts/app.js
@@ -1,2 +1,12 @@
 ﻿/// <reference path="jquery.js" />
 /// <reference path="bootstrap.js" />
+$(document).ready(() => {
+    $('#accountType').change(() => {
+        const value = $('#accountType').val();
+        if (value === 'admin') {
+            $('.paymentInput').show();
+        } else {
+            $('.paymentInput').hide();
+        }
+    });
+});
