c424fb4def2bbbd0e9698262004a2ad82fa58574
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-19 15:49:41 +0100

    ft(template-centerDetails.html)
    *Create and design centerDetails page using dashboard template
    *Create and design table section to display facilities of the center
    *Use javascript to dynamically modify addCenter modal to become modifyCenter modal
    *Use javascript to dynamically link centerDetails page to each table row in MyCenters page

--- Template/resources/scripts/app.js
+++ Template/resources/scripts/app.js
@@ -9,10 +9,56 @@ $(document).ready(() => {
             $('.paymentInput').hide();
         }
     });
-    $('.createEvent').click(() => {
-        window.location = 'MyEvents.html';
+    $('#deleteEvent').click(() => {
+        const sure = confirm('Are you sure you want to delete Event?');
+        if (sure) {
+            alert('Event Deleted');
+            window.location = 'MyEvents.html';
+        }
     });
-    $('.createCenter').click(() => {
-        window.location = 'MyCenters.html';
+    $('#deleteCenter').click(() => {
+        const sure = confirm('Are you sure you want to delete Center?');
+        if (sure) {
+            alert('Center Deleted');
+            window.location = 'MyCenters.html';
+        }
     });
+
+    $('#addEvent').click(() => {
+        $('.createEvent').text('Create');
+        $('#addNewEventTitle').text('Add Event');
+        $('.createEvent').click(() => {
+            window.location = 'MyEvents.html';
+        });
+    });
+    $('#addCenter').click(() => {
+        $('.createCenter').text('Create');
+        $('#addNewCenterTitle').text('Add Center');
+        $('.createCenter').click(() => {
+            window.location = 'MyCenters.html';
+        });
+    });
+
+    $('.event').click(() => {
+        window.location = 'ManageEvent.html';
+    });
+    $('.center').click(() => {
+        window.location = 'CenterDetails.html';
+    });
+
+    $('#editEvent').click(() => {
+        $('.createEvent').text('Save');
+        $('#addNewEventTitle').text('Modify Event');
+        $('.createEvent').click(() => {
+            $('#addNewEvent').modal('hide');
+        });
+    });
+    $('#editCenter').click(() => {
+        $('.createCenter').text('Save');
+        $('#addNewCenterTitle').text('Modify Center');
+        $('.createCenter').click(() => {
+            $('#addNewCenter').modal('hide');
+        });
+    });
+
 });
