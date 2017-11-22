28b79908517b43a73ace22bc2666ab33292a63be
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-22 21:15:33 +0100

    Implement route POST /events/
    *create validator middleware to validate request
    *create events model
    *create eventRouter

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,7 @@
 ﻿import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import events from './routers/eventRouter';
 
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve('./././template')));
+app.use('/api/v1/events', events);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./././template/index.html'));
---
+++ Server/routers/eventRouter.js
@@ -0,0 +1,10 @@
+﻿import express from 'express';
+import Events from '../controllers/eventController';
+import val from '../middlewares/validator';
+
+
+const Validator = new val('events');
+const router = express.Router();
+
+router.post('/', Validator.verify, Events.postEvent);
+export default router;
