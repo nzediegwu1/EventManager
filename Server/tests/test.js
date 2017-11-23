73453c714928302da683091077cd4e455494b8b7
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-23 14:51:13 +0100

    Install test libraries as devDependencies
    *install mocha
    *install chai
    *install chai-http

---
+++ Server/tests/test.js
@@ -0,0 +1,6 @@
+﻿import mocha from 'mocha';
+import chai from 'chai';
+import EventController from '../controllers/eventController.js';
+import CenterController from '../controllers/centerController.js';
+
+const should = chai.should;
