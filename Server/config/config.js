a2490f2222252f4fbbb82fdefccac551c0e2315e
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-27 18:25:20 +0100

    Exclude .env file

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,22 +1,20 @@
-﻿require('dotenv').config();
-
-module.exports = {
-  development: {
-    username: 'admin',
-    password: 'password1',
-    database: 'EventManager',
-    host: '127.0.0.1',
-    dialect: 'postgres',
-  },
-  test: {
-    username: 'admin',
-    password: 'password1',
-    database: 'EventManager',
-    host: '127.0.0.1',
-    dialect: 'postgres',
-  },
-  production: {
-      use_env_variable: process.env.DATABASE_URL,
-      dialect: 'postgres',
-  },
+﻿module.exports = {
+    development: {
+        username: 'admin',
+        password: 'password1',
+        database: 'EventManager',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    test: {
+        username: 'admin',
+        password: 'password1',
+        database: 'EventManager',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    production: {
+        use_env_variable: 'DATABASE_URL_PRODUCTION',
+        dialect: 'postgres',
+    },
 };
