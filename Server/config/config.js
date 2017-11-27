8b1fc3d0f049584dba499fdec559bb1986cffbc5
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-27 08:50:35 +0100

    fixing heroku migration problem
    manually enter database url

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,6 +1,6 @@
 ﻿require('dotenv').config();
 
-const databaseUrl = process.env.DATABASE_URL;
+// const databaseUrl = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
@@ -18,7 +18,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: databaseUrl,
+    use_env_variable: 'postgres://xcnwqlat:zmYX6GdPo_2tzznPiKoYSCbfPXp_Cci7@baasu.db.elephantsql.com:5432/xcnwqlat',
     dialect: 'postgres',
   },
 };
