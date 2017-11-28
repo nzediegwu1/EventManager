24720aa7c6a57b88328246fca0024127e66e3440
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-28 01:13:54 +0100

    Implement user signin API endpoint
    Create POST: /users/login in usersRouter
    Define login method in usersController
    Create token for user signin using jwt
    Implement validation for user signin route

--- Server/routers/userRouter.js
+++ Server/routers/userRouter.js
@@ -3,10 +3,12 @@ import UsersController from '../controllers/userController';
 import val from '../middlewares/validator';
 
 const signupValidator = new val('users', 'signup');
+const siginValidator = new val('users', 'signin');
 const user = new UsersController();
 const router = express.Router();
 
 router.post('/', signupValidator.verify, user.signUp);
+router.post('/login', siginValidator.verify, user.signIn);
 router.get('/', user.getUsers);
 
 export default router;
