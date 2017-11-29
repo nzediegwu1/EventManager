b3a12be44163466a4d446755307ee87c9f22b567
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-29 11:58:27 +0100

    change database connection

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,9 +1,10 @@
 ﻿module.exports = {
     development: {
-        username: 'admin',
-        password: 'password1',
-        database: 'EventManager',
-        host: '127.0.0.1',
+        username: 'txvdyccl',
+        password: 'w2YlzmPt5T5SvrFbjDnnZ9bGb6Vd0Ttl',
+        database: 'txvdyccl',
+        host: 'tantor.db.elephantsql.com',
+        port: '5432',
         dialect: 'postgres',
     },
     test: {
