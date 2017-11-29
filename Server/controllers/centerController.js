574880e16d3f8d2c96b49dedd33a875d72f40b9c
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-29 22:05:35 +0100

    Test for add event endpoint 201 status code

--- Server/controllers/centerController.js
+++ Server/controllers/centerController.js
@@ -7,45 +7,55 @@ const model = models.Centers;
 class Centers {
     // add an event
     addCenter(req, res) {
-        return model.findAll()
-            .then(centers => { // destructuring
-                const { name, address, location, capacity, price, picture } = req.body;
-                if (centers.length !== 0) {
-                    centers.forEach(center => {
-                        if (center.name === name
-                            && center.location === location
-                            && center.address === address) {
-                            // unacceptable
-                            return validator.response(res, 'err', 406, 'Center already exists');
+        models.Users.findById(req.decoded.id).then(user => {
+            if (user.accountType === 'admin') {
+                return model.findAll()
+                    .then(centers => { // destructuring
+                        const { name, address, location, capacity, price, picture } = req.body;
+                        if (centers.length !== 0) {
+                            centers.forEach(center => {
+                                if (center.name === name
+                                    && center.location === location
+                                    && center.address === address) {
+                                    // unacceptable
+                                    return validator.response(res, 'err', 406, 'Center already exists');
+                                }
+                            });
                         }
-                    });
-                }
-                const newEntry = { name, address, location, userId: req.decoded.id, picture,
-                    capacity: parseInt(capacity), price: parseInt(price),
-                };
-                return model.create(newEntry)
-                     .then(created => validator.response(res, 'success', 201, created))
-                     .catch(error => validator.response(res, 'error', 500, error));
-            }).catch(error => validator.response(res, 'error', 500, error));
+                        const newEntry = { name, address, location, userId: req.decoded.id, picture,
+                            capacity: parseInt(capacity), price: parseInt(price),
+                        };
+                        return model.create(newEntry)
+                             .then(created => validator.response(res, 'success', 201, created))
+                             .catch(error => validator.response(res, 'error', 500, error));
+                    }).catch(error => validator.response(res, 'error', 500, error));
+            }
+            return validator.response(res, 'error', 403, 'Only an admin can perform this action');
+        }).catch(error => validator.response(res, 'error', 500, error));
     }
 
     // modify a center
     modifyCenter(req, res) {
         // get center with same index as parameter and change the value
         if (validator.confirmParams(req, res)) { // destructuring
-            const { name, address, location, capacity, price, picture } = req.body;
-            const modifiedEntry = { name, address, location, picture, userId: req.decoded.id,
-                capacity: parseInt(capacity), price: parseInt(price) };
-            return model.update(modifiedEntry, { where: { id: req.params.id, userId: req.decoded.id } })
-               .then(updatedCenter => {
-                   if (updatedCenter[0] === 1) {
-                       return validator.response(res, 'success', 202, 'Update successful');
-                   }
-                   // trying to update a center whose id does not exist
-                   // and or which doesnt belong to the user
-                   return validator.response(res, 'error', 406, 'Invalid transaction');
-               })
-               .catch(error => validator.response(res, 'error', 500, error));
+            models.Users.findById(req.decoded.id).then(user => {
+                const { name, address, location, capacity, price, picture } = req.body;
+                const modifiedEntry = { name, address, location, picture, userId: req.decoded.id,
+                    capacity: parseInt(capacity), price: parseInt(price) };
+
+                if (user.accountType === 'admin') {
+                    return model.update(modifiedEntry, { where: { id: req.params.id, userId: req.decoded.id } })
+                       .then(updatedCenter => {
+                           if (updatedCenter[0] === 1) {
+                               return validator.response(res, 'success', 202, 'Update successful');
+                           }
+                           // trying to update a center whose id does not exist
+                           // and or which doesnt belong to the user
+                           return validator.response(res, 'error', 406, 'Invalid transaction');
+                       }).catch(error => validator.response(res, 'error', 500, error));
+                }
+                return validator.response(res, 'error', 403, 'Only an admin can perform this action');
+            }).catch(error => validator.response(res, 'error', 500, error));
         }
         return validator.invalidParameter;
     }
@@ -72,8 +82,7 @@ class Centers {
                     return validator.response(res, 'success', 200, center);
                 }
                 return validator.response(res, 'error', 404, 'Could not find Center');
-            })
-            .catch(error => validator.response(res, 'error', 500, error));
+            }).catch(error => validator.response(res, 'error', 500, error));
         }
         return validator.invalidParameter;
     }
--- Server/tests/test.js
+++ Server/tests/test.js
@@ -5,159 +5,160 @@ import app from '../app';
 chai.use(chaiHttp);
 
 describe('Tests for EventRouter', () => {
-    /* it('Should test for addEvents 201 response status code', (done) => {
-         chai.request(app)
-           .post('/api/v1/events')
-           .send({
-               title: 'Andela Bootcamp',
-               date: 'March 21, 2012',
-               time: '8:30',
-               venue: 'Andela Epic Tower',
-               description: 'A technology learning program',
-           })
-           .end((err, res) => {
-               expect(res).to.have.status(201);
-               done();
-           });
-     });
-  
-    it('Should test getEvents 200 response status code', (done) => {
-      chai.request(app)
-        .get('/api/v1/events')
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          done();
-        });
-    });
-  
-    it('Should test modifyEvent 404 error response status code', (done) => {
+    it('Should test for addEvents 201 response status code', (done) => {
         chai.request(app)
-        .put('/api/v1/events/4')
+          .post('/api/v1/events')
           .send({
               title: 'Andela Bootcamp',
-              date: 'March 21, 2012',
+              date: 'March 21, 2010',
               time: '8:30',
-              venue: 'Andela Epic Tower',
+              centerId: '2',
               description: 'A technology learning program',
-          })
-        .end((err, res) => {
-            expect(res).to.have.status(404);
-            done();
-        });
-    });
-  
-    it('Should test modifyEvent 202 success response status code', (done) => {
-        chai.request(app)
-          .put('/api/v1/events/0')
-          .send({
-              title: 'Andela Bootcamp',
-              date: 'March 21, 2012',
-              time: '8:30',
-              venue: 'Andela Epic Tower',
-              description: 'A technology learning program',
-          })
-          .end((err, res) => {
-              expect(res).to.have.status(202);
-              done();
-          });
-    });
-  });
-  
-  describe('Tests for CenterRouter', () => {
-    it('Should test for addCenter 201 response status code', (done) => {
-        chai.request(app)
-          .post('/api/v1/centers')
-          .send({
-              name: 'Eagle square',
-              address: 'Ikorodu Road',
-              location: 'Lagos',
-              capacity: '1000',
-              price: '25000',
+              token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNTExOTY5NDg2LCJleHAiOjE1MTIwNTU4ODZ9.Zzb7FBm42wiQJ7qNTyJyjecgg1lTj88lXqMEOzqWUZM',
           })
           .end((err, res) => {
               expect(res).to.have.status(201);
               done();
           });
     });
-  
-  
-    it('Should test for addCenter 406 response status code', (done) => {
-        chai.request(app)
-          .post('/api/v1/centers')
-          .send({
-              name: 'Andela Epic Tower',
-              address: 'Ikorodu Road',
-              location: 'Lagos',
-              capacity: '1000',
-              price: '25000',
-          })
-          .end((err, res) => {
-              expect(res).to.have.status(406);
-              done();
-          });
-    });
-  
-    it('Should test for modifyCenter 200 response status code', (done) => {
-        chai.request(app)
-          .put('/api/v1/centers/0')
-          .send({
-              name: 'Andela Epic Tower',
-              address: 'Ikorodu Road',
-              location: 'Lagos',
-              capacity: '1000',
-              price: '25000',
-          })
-          .end((err, res) => {
-              expect(res).to.have.status(200);
-              done();
-          });
-    });
-    it('Should test for modifyCenter 404 response status code', (done) => {
-        chai.request(app)
-          .put('/api/v1/centers/9')
-          .send({
-              name: 'Andela Epic Tower',
-              address: 'Ikorodu Road',
-              location: 'Lagos',
-              capacity: '1000',
-              price: '25000',
-          })
+    /*
+      it('Should test getEvents 200 response status code', (done) => {
+          chai.request(app)
+            .get('/api/v1/events')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+      });
+    
+      it('Should test modifyEvent 404 error response status code', (done) => {
+          chai.request(app)
+          .put('/api/v1/events/4')
+            .send({
+                title: 'Andela Bootcamp',
+                date: 'March 21, 2012',
+                time: '8:30',
+                venue: 'Andela Epic Tower',
+                description: 'A technology learning program',
+            })
           .end((err, res) => {
               expect(res).to.have.status(404);
               done();
           });
-    });
-    it('Should test for getAllCenters 200 response status code', (done) => {
-      chai.request(app)
-        .get('/api/v1/centers')
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          done();
-        });
-    });
-    it('Should test for getCenterDetails 200 response status code', (done) => {
-        chai.request(app)
-          .get('/api/v1/centers/0')
-          .end((err, res) => {
-              expect(res).to.have.status(200);
-              done();
-          });
-    });
-    it('Should test for getCenterDetails 404 response status code', (done) => {
-        chai.request(app)
-          .get('/api/v1/centers/7')
-          .end((err, res) => {
-              expect(res).to.have.status(404);
-              done();
-          });
-    });
-  
-    it('Should test for getCenterDetails for invalid parameter', (done) => {
-      chai.request(app)
-        .get('/api/v1/centers/fdf')
-        .end((err, res) => {
-          expect(res).to.have.status(400);
-          done();
-        });
-    });   */
+      });
+    
+      it('Should test modifyEvent 202 success response status code', (done) => {
+          chai.request(app)
+            .put('/api/v1/events/0')
+            .send({
+                title: 'Andela Bootcamp',
+                date: 'March 21, 2012',
+                time: '8:30',
+                venue: 'Andela Epic Tower',
+                description: 'A technology learning program',
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(202);
+                done();
+            });
+      });
+  });
+    
+  describe('Tests for CenterRouter', () => {
+      it('Should test for addCenter 201 response status code', (done) => {
+          chai.request(app)
+            .post('/api/v1/centers')
+            .send({
+                name: 'Eagle square',
+                address: 'Ikorodu Road',
+                location: 'Lagos',
+                capacity: '1000',
+                price: '25000',
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(201);
+                done();
+            });
+      });
+    
+    
+      it('Should test for addCenter 406 response status code', (done) => {
+          chai.request(app)
+            .post('/api/v1/centers')
+            .send({
+                name: 'Andela Epic Tower',
+                address: 'Ikorodu Road',
+                location: 'Lagos',
+                capacity: '1000',
+                price: '25000',
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(406);
+                done();
+            });
+      });
+    
+      it('Should test for modifyCenter 200 response status code', (done) => {
+          chai.request(app)
+            .put('/api/v1/centers/0')
+            .send({
+                name: 'Andela Epic Tower',
+                address: 'Ikorodu Road',
+                location: 'Lagos',
+                capacity: '1000',
+                price: '25000',
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+      });
+      it('Should test for modifyCenter 404 response status code', (done) => {
+          chai.request(app)
+            .put('/api/v1/centers/9')
+            .send({
+                name: 'Andela Epic Tower',
+                address: 'Ikorodu Road',
+                location: 'Lagos',
+                capacity: '1000',
+                price: '25000',
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(404);
+                done();
+            });
+      });
+      it('Should test for getAllCenters 200 response status code', (done) => {
+          chai.request(app)
+            .get('/api/v1/centers')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+      });
+      it('Should test for getCenterDetails 200 response status code', (done) => {
+          chai.request(app)
+            .get('/api/v1/centers/0')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+      });
+      it('Should test for getCenterDetails 404 response status code', (done) => {
+          chai.request(app)
+            .get('/api/v1/centers/7')
+            .end((err, res) => {
+                expect(res).to.have.status(404);
+                done();
+            });
+      });
+    
+      it('Should test for getCenterDetails for invalid parameter', (done) => {
+          chai.request(app)
+            .get('/api/v1/centers/fdf')
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                done();
+            });
+      }); */
 });
