d371842aab70b1d766ea1caf75054fe0185c5112
nzediegwu1 <nzediegwu1@outlook.com>
2017-12-01 04:51:17 +0100

    User controller tests

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,18 +1,10 @@
 ﻿module.exports = {
     development: {
-        /*
         username: 'admin',
         password: 'password1',
         database: 'EventManager',
         host: '127.0.0.1',
-        dialect: 'postgres',*/
-        username: 'omkcdhmt',
-        password: 'rxAH5VckpPKKQlg0dGyk5fbELSipPo3X',
-        database: 'omkcdhmt',
-        host: 'baasu.db.elephantsql.com',
-        port: '5432',
         dialect: 'postgres',
-
     },
     test: {
         username: 'omkcdhmt',
