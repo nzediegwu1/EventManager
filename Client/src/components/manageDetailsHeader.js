1e1625d24921f576fe0587c088ee55d4166c726b
nzediegwu1 <nzediegwu1@outlook.com>
2018-03-01 19:55:41 +0100

    Consume API endpoint for deleting an event
    1. Implement logic to send request to a delete a resource given pageType, param and picture props
    2. Implement logic in controller delete action to delete image file from /public/events directory upon successful delete request

--- Client/src/components/manageDetailsHeader.js
+++ Client/src/components/manageDetailsHeader.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import editIcon from '../resources/images/glyphicons-151-edit.png';
 import removeIcon from '../resources/images/glyphicons-17-bin.png';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 const mapStateToProps = state => {
   let currentPage = state.page.currentPage;
   let owner = (currentPage === 'centerDetails') && (state.centers.centerDetails[0]) ?
     state.centers.centerDetails[0].user.id : (currentPage === 'manageEvent') && state.events.event[0] && state.events.event[0].user.id;
   return {
-    owner: owner
+    owner: owner,
+    currentPage: state.page.currentPage,
+    eventDetails: state.events.event,
+    centerDetails: state.centers.centerDetails    
   };
 };
 const Manager = (props) => {
@@ -18,7 +22,7 @@ const Manager = (props) => {
         <button type="submit" id="editEvent" className="btn btn-success" data-toggle="modal" data-target={props.editModal}>
           <img src={editIcon} alt="Edit" />
         </button>
-        <button type="submit" className="btn btn-danger icon-margin-left" id="deleteEvent">
+        <button onClick={props.deleteEvent} type="submit" className="btn btn-danger icon-margin-left" id="deleteEvent">
           <img src={removeIcon} alt="delete" />
         </button>
       </div>
@@ -26,8 +30,32 @@ const Manager = (props) => {
   );
   return content;
 }
+let param, currentPage, history, resource;
 class ManageDetails extends Component {
+  constructor(props) {
+    super(props)
+    this.delete = this.delete;
+  }
+  delete() {
+    const urlGenerator = () => {
+      const type = (currentPage === 'manageEvent') ? 'events' : 'centers';
+      return `http://localhost:8080/api/v1/${type}/${param}/?token=${JSON.parse(localStorage.token).value}&file=${resource.picture}`;
+    }
+    const url = urlGenerator();
+    axios.delete(url).then(res => {
+      alert(res.data.data)
+      history.push('/dashboard/events');
+    }).catch(err => {
+      alert(err.response.data.message);
+      (err.response.status === 403 || err.response.status === 401) && logout('addNewEvent', history);
+    });
+  }
   render() {
+    param = this.props.param;
+    currentPage = this.props.currentPage;
+    history = this.props.history;
+    resource = (currentPage === 'manageEvent') ? this.props.eventDetails[0] : this.props.centerDetails[0];
+
     const content = (
       <table className="table-responsive col-sm-12 bg-transparent  zero-padding">
         <tbody>
@@ -35,7 +63,7 @@ class ManageDetails extends Component {
             <td style={{ width: '80%' }}><h4 className="text-center"><b>{this.props.title}</b></h4></td>
             {
               (JSON.parse(localStorage.token).id === this.props.owner)
-              && <Manager editModal={this.props.editModal} />
+              && <Manager deleteEvent={this.delete} editModal={this.props.editModal} />
             }
           </tr>
         </tbody>
