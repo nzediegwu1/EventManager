a8ab2bd079f02e7aaa230be49cb8bccc5abdbeaf
nzediegwu1 <nzediegwu1@outlook.com>
2018-03-09 20:19:40 +0100

    change webpack-dev-server to use dynamic port

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ console.log(`process.env.PORT: ${process.env.PORT}`);
 module.exports = {
   entry: [
     'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:39220',
+    `webpack-dev-server/client?http://localhost:${process.env.PORT}`,
     'webpack/hot/only-dev-server',
     './Client/src/app.js',
   ],
