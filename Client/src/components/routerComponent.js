bdc4d431351405b06145445b5e31898358f551b6
nzediegwu1 <nzediegwu1@outlook.com>
2018-03-12 16:43:17 +0100

    Make correction in apiLink

--- Client/src/components/routerComponent.js
+++ Client/src/components/routerComponent.js
@@ -3,7 +3,7 @@ import { SignIn } from './signinComponent';
 import { Dashboard } from './dashboardComponent';
 import { Route, Switch } from 'react-router-dom';
 
-const apiLink = 'https://eventmanageronline.herokuapp.com/';
+const apiLink = 'https://eventmanageronline.herokuapp.com';
 localStorage.setItem('apiLink', apiLink);
 
 export const App = () => (
